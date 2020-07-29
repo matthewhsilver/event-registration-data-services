dfb530e2d088e9f6442be99a719c72bf0e58fc0b
Matthew Silver <msilver@ncl.com>
2020-07-29 13:52:43 -0400

    Initial commit

---
+++ src/main/java/com/webage/eventregistration/dataservices/DataServicesApplication.java
@@ -0,0 +1,13 @@
+package com.webage.eventregistration.dataservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webage/eventregistration/dataservices/DataServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webage.eventregistration.dataservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
