1a08e0b051cf663b852066af8161c60095c469ec
wasadmin <wasadmin@192.168.68.154>
2020-07-30 16:41:07 -0400

    Added utility class.

---
+++ src/main/java/com/webage/eventregistration/dataservices/util/Utils.java
@@ -0,0 +1,9 @@
+package com.webage.eventregistration.dataservices.util;
+
+public class Utils {
+
+	public static boolean isEmpty(String value) {
+		return value == null || value.trim().isEmpty();
+	}
+	
+}
