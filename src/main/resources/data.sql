593cb0bb46be932cf3c3d555948d344801e2c52b
wasadmin <wasadmin@192.168.68.154>
2020-07-30 15:45:31 -0400

    Migrated to Spring JPA.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CUSTOMERS (USER_NAME, EMAIL, PASSWORD) VALUES ('jsmith', 'jsmith@gmail.com', 'jsmith');
+insert into CUSTOMERS (USER_NAME, EMAIL, PASSWORD) VALUES ('lhamilton', 'lhamilton@gmail.com', 'lhamilton');
+insert into CUSTOMERS (USER_NAME, EMAIL, PASSWORD) VALUES ('bjones', 'bjones@gmail.com', 'bjones');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+-- ----------------------------------------------
+-- DDL Statements for tables
+-- ----------------------------------------------
+
+CREATE TABLE "CUSTOMERS" (
+	ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
+	USER_NAME VARCHAR(255), 
+	EMAIL VARCHAR(255),
+	PASSWORD VARCHAR(255));
+-- ----------------------------------------------
+-- DDL Statements for keys
+-- ----------------------------------------------
+
+-- primary/unique
+ALTER TABLE "CUSTOMERS" ADD CONSTRAINT "CUSTOMERS_PK" PRIMARY KEY ("ID");
